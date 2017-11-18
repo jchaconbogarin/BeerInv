33db57fbde90b8fa83a776b0abaf1e2fd853ae48
Jose Chacón <jchaconbogarin@gmail.com>
2017-11-18 17:39:35 -0600

    Added migrations and Breweries model

--- models.py
+++ models.py
@@ -2,3 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
 
 db = SQLAlchemy()
+
+class Brewery(db.Model):
+    __tablename__ = 'breweries'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return '<Brewery id={} name={}>'.format(self.id, self.name)
