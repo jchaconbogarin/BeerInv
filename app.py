337db39f6633b7da00991c3261a3462eccc74e9b
Jose Chacón <jchaconbogarin@gmail.com>
2017-11-18 16:21:46 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from models import db
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Boga's beer collection inventory"
+
+if __name__ == '__main__':
+    app.run()
