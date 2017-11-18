a94ebe628dfaef2c4ccecbcd4e855d38882e9e4e
Jose Chacón <jchaconbogarin@gmail.com>
2017-11-18 16:44:08 -0600

    Minor changes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     return "Boga's beer collection inventory"
 
