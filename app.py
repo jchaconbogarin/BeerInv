3ef5991a4500b49d1f48f5948ef414692322c5d0
Jose Chacón <jchaconbogarin@gmail.com>
2017-11-23 20:56:13 -0600

    Gunicorn fix

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 
 from beerinv import initApp
 
+app = initApp(os.environ['APP_SETTINGS'])
+
 if __name__ == '__main__':
     
-    app = initApp(os.environ['APP_SETTINGS'])
     app.run()
